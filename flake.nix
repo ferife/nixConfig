18f02c29d1ceafdcea32b87728797cd2ee4218f7
Fernando RF <ferife@icloud.com>
2024-10-10 04:36:10 -0500

    Add more Todos

--- flake.nix
+++ flake.nix
@@ -36,11 +36,15 @@
 }
 
 # TODO: Implement Home Manager
+  # Link for searching home manager options: https://home-manager-options.extranix.com/
 # TODO: Make my configuration more modular
 # TODO: Add back all the packages I was using
-# TODO: Configure GNOME
+# TODO: Configure GNOME - Use Home Manger for this
 # TODO: Figure out if there's a way to add my Obsidian plugin & settings setup to my flake, or at least to a git repo
 # TODO: Figure out gaming on NixOS
 # TODO: Make this a remote repo
 # TODO: Set up a password management solution
 # TODO: Cancel my subsctiption to ExpressVPN
+# TODO: Set up NixOS auto update
+# TODO: Set up auto update for all my applications
+# TODO: Look into Manix CLI
