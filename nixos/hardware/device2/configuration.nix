4a8f560fc5d53523b12de99d9c1ff0c15417ffca
Fernando RF <ferife@icloud.com>
2024-10-22 10:45:23 -0500

    Rename files for consistent naming convention

--- nixos/hardware/device2/configuration.nix
+++ nixos/hardware/device2/configuration.nix
@@ -10,15 +10,15 @@
       # Include the results of the hardware scan.
       ./hardware-configuration.nix
 
-      ../../modules/autoupgrade.nix
+      ../../modules/auto-upgrade.nix
 
       # Application Modules
-      ../../modules/packages/accessMedia.nix
-      ../../modules/packages/cliTools.nix
-      ../../modules/packages/createMedia.nix
+      ../../modules/packages/access-media.nix
+      ../../modules/packages/cli-tools.nix
+      ../../modules/packages/create-media.nix
       # ../../modules/packages/distrobox.nix
       ../../modules/packages/mullvad-vpn.nix
-      ../../modules/packages/requiredForProgramming.nix
+      ../../modules/packages/programming.nix
       ../../modules/packages/thunderbird.nix
       ../../modules/packages/tor.nix
     ];
