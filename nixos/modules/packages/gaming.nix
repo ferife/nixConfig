f9e6556abb2b793763c6bedce9e401cf86cbd1ad
Fernando RF <ferife@icloud.com>
2024-10-26 10:46:20 -0500

    Combine openrazer.nix and gaming.nix

--- nixos/modules/packages/gaming.nix
+++ nixos/modules/packages/gaming.nix
@@ -2,15 +2,16 @@
 
 { config, pkgs, ... }:
 {
-  imports = [
-    ./openrazer.nix # Support & Configure Razer-brand peripheral usage
-  ];
-
   programs.steam = {
     enable = true;
   };
-
+  
+  # The following lines are required for OpenRazer
+  hardware.openrazer.enable = true; # Enable OpenRazer drivers
   environment.systemPackages = with pkgs; [
-    
+    openrazer-daemon  # self-explanatory
+    polychromatic     # front-end for controlling Razer peripheral LEDs
   ];
+  # Add a copy of the following line for each user that needs to use the openrazer-daemon
+  users.users.fernandorf.extraGroups = [ "openrazer" ];
 }
