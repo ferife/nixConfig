ab258356d22cb19826cca2834a18a0e64885f652
Fernando RF <ferife@icloud.com>
2024-10-28 08:25:55 -0500

    Rearrange gaming.nix and add input remapper

--- nixos/modules/packages/gaming.nix
+++ nixos/modules/packages/gaming.nix
@@ -8,10 +8,19 @@
   
   # The following lines are required for OpenRazer
   hardware.openrazer.enable = true; # Enable OpenRazer drivers
+  users.users.fernandorf.extraGroups = [ "openrazer" ];
+
+  services.input-remapper = {
+    enable = true;
+  };
+
   environment.systemPackages = with pkgs; [
+    
+    # OpenRazer packages
     openrazer-daemon  # self-explanatory
     polychromatic     # front-end for controlling Razer peripheral LEDs
+  
+    # Other
+    
   ];
-  # Add a copy of the following line for each user that needs to use the openrazer-daemon
-  users.users.fernandorf.extraGroups = [ "openrazer" ];
 }
