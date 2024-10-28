52edde0f4a4ed387f32a4699925f7955db76d25f
Fernando RF <ferife@icloud.com>
2024-10-28 09:18:57 -0500

    Add message to the device2 files about remembering to set them up once there is a second device

--- nixos/hosts/device2/configuration.nix
+++ nixos/hosts/device2/configuration.nix
@@ -1,25 +1,11 @@
 { config, pkgs, ... }:
 
 {
+  # CHANGE ONCE THIS CONFIG STARTS TO BE USED IN A SECOND HOST  
+  
+
   imports =
     [ 
-      # Add general config options required for the computer to work
-      ../../general-config.nix
-
-
-      # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-
-      ../../modules/settings/auto-upgrade.nix
-
-      # Application Modules
-      ../../modules/packages/access-media.nix
-      ../../modules/packages/cli-tools.nix
-      ../../modules/packages/create-media.nix
-      # ../../modules/packages/distrobox.nix
-      ../../modules/packages/mullvad-vpn.nix
-      ../../modules/packages/programming.nix
-      ../../modules/packages/thunderbird.nix
-      ../../modules/packages/tor.nix
+      ./../laptop/configuration.nix
     ];
 }
