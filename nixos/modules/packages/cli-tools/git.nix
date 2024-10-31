9dbbb0e23ba294e982606e3c81f8baad20911ea9
Fernando RF <ferife@icloud.com>
2024-10-31 09:58:30 -0500

    Add comment to git.nix

--- nixos/modules/packages/cli-tools/git.nix
+++ nixos/modules/packages/cli-tools/git.nix
@@ -4,6 +4,8 @@
 		gitModule.enable = lib.mkEnableOption "Installs & configures Git";
 	};
 
+	# When I move this to Home Manager, I'll have more options available, such as declaring the user's name and email
+
 	config = lib.mkIf config.gitModule.enable {
 		programs.git = {
       enable = true;
