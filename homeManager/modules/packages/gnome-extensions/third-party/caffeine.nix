8d970a88f33fc066285ec45d196c420dea0fefed
Fernando RF <ferife@icloud.com>
2024-11-06 11:44:39 -0600

    Add commented code to test later

--- homeManager/modules/packages/gnome-extensions/third-party/caffeine.nix
+++ homeManager/modules/packages/gnome-extensions/third-party/caffeine.nix
@@ -5,6 +5,7 @@
 
   options = {
     caffeineModule.enable = lib.mkEnableOption "Installs & enables the Caffeine extension, which allows the screen to stay on";
+    # caffeineModule.activate = lib.mkEnableOption "Causes Caffeine to activate on startup with no timer";
   };
 
   config = lib.mkIf config.caffeineModule.enable {
@@ -18,6 +19,10 @@
           "caffeine@patapon.info"
         ];
       };
+
+      # "org/gnome/shell/extensions/caffeine" = {
+      #   toggle-state = lib.mkIf config.caffeineModule.activate true;
+      # };
     };
   };
 }
--- homeManager/modules/packages/gnome-extensions/third-party/module-bundle.nix
+++ homeManager/modules/packages/gnome-extensions/third-party/module-bundle.nix
@@ -9,7 +9,11 @@
     ./weather-oclock.nix
   ];
 
-  caffeineModule.enable = lib.mkDefault true;
+  # caffeineModule.enable = lib.mkDefault true;
+  caffeineModule = {
+    enable = lib.mkDefault true;
+    activate = lib.mkDefault false;
+  };
   forgeModule.enable = lib.mkDefault true;
   tilingShellModule.enable = lib.mkDefault false;
   weatherOclockModule.enable = lib.mkDefault true;
