2ecfbee8b37a883025b2557dfdf17f731abeb2f1
Fernando RF <ferife@icloud.com>
2024-11-13 09:03:33 -0600

    Fix Typo

--- nixos/modules/settings/user-config.nix
+++ nixos/modules/settings/user-config.nix
@@ -5,7 +5,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.fernandorf = {
     isNormalUser = true;
-    description = "Fernando RiveraFernadez";
+    description = "Fernando Rivera Fernandez";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
 
