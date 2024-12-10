eec5d1ab8c8150e1996738a0de4348428be7f74e
Fernando RF <ferife@icloud.com>
2024-12-10 06:00:05 -0600

    Change nvim welcome message

--- homeManager/modules/packages/cli-tools/advent-of-nvim/init.lua
+++ homeManager/modules/packages/cli-tools/advent-of-nvim/init.lua
@@ -1,3 +1,3 @@
 -- This config was created in part by following the "Advent of Neovim" series of YouTibe videos by TJ DeVries
 
-print("Hello World") -- This worked, it printed at the bottom of nvim
+print("Welcome to FRF's Advent of NVim Configuration") -- This worked, it printed at the bottom of nvim
