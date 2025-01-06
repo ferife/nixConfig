9c9e330545372f5b37e87d9a250aebaac1f676a1
Fernando RF <ferife@icloud.com>
2025-01-06 06:04:06 -0600

    Add mini.nvim modules

--- homeManager/modules/packages/cli-tools/advent-of-nvim/lua/config/plugins/mini.lua
+++ homeManager/modules/packages/cli-tools/advent-of-nvim/lua/config/plugins/mini.lua
@@ -4,6 +4,47 @@ return {
     -- Library of 40+ independent Lua modules making small QoL changes to Neovim
 
     config = function()
+      -- The following adds animations to some basic Neovim actions, such as moving the cursor
+      -- local animate = require 'mini.animate'
+      -- animate.setup()
+
+      -- The following adds an animated vertical line that indicates scope, based on the number of indents
+      local indentscope = require 'mini.indentscope'
+      indentscope.setup()
+
+      -- The following adds the ability to easily move text up or down, without having to cut and paste it
+      -- It also allows for moving left and right, meaning to add/remove indents
+      local move = require 'mini.move'
+      move.setup({
+        mappings = {
+          -- Move visual selection in normal mode
+          -- Default (shown below) are Alt (Meta) + hjkl
+          left = '<M-h>',
+          down = '<M-j>',
+          up = '<M-k>',
+          right = '<M-l>',
+
+          -- Move current line in normal mode
+          -- Default (shown below) are Alt (Meta) + hjkl
+          line_left = '<M-h>',
+          line_down = '<M-j>',
+          line_up = '<M-k>',
+          line_right = '<M-l>',
+        }
+      })
+
+      -- The following will autoclose parentheses, braces, brackets, single-quotes, double-quotes, and backticks (`)
+      -- When creating a newline from between the opening and closing symbols,
+      --  it will automatically newline again for the closing symbol
+      local pairs = require 'mini.pairs'
+      pairs.setup()
+
+      -- The following adds the ability to toggle whether the of a table/array/dictionary are all on the same line
+      -- or if they each have their own line
+      -- Use `gS` to toggle
+      local splitjoin = require 'mini.splitjoin'
+      splitjoin.setup()
+
       -- The following adds a pretty status line at the bottom of the window containing various important pieces of information
       local statusline = require 'mini.statusline'
       statusline.setup { use_icons = true }
