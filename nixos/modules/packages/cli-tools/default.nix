590eeebc16b730ca9168f762ebae9402d97666be
Fernando RF <ferife@icloud.com>
2025-01-12 23:45:54 -0600

    Begin work oon password-store

--- nixos/modules/packages/cli-tools/default.nix
+++ nixos/modules/packages/cli-tools/default.nix
@@ -8,11 +8,13 @@
     ./git.nix
     ./neofetch.nix
     ./neovim.nix
+    ./password-store.nix
   ];
 
   bashNixosModule.enable = lib.mkDefault true;
   gitModule.enable = lib.mkDefault true;
   neofetchModule.enable = lib.mkDefault true;
+  passNixosModule.enable = lib.mkDefault true;
   
   distroboxModule.enable = lib.mkDefault false;
 }
--- nixos/modules/packages/other/default.nix
+++ nixos/modules/packages/other/default.nix
@@ -3,8 +3,10 @@
 { config, lib, pkgs, ... }:
 {
   imports = [
+    ./firefox.nix
     ./mullvad-vpn.nix
   ];
 
+  firefoxNixosModule.enable = lib.mkDefault true;
   mullvadVpnModule.enable = lib.mkDefault true;
 }
