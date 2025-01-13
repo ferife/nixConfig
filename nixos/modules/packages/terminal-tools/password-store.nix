4fd538cdda191bb96f54aa751f382308f3217902
Fernando RF <ferife@icloud.com>
2025-01-12 23:45:54 -0600

    Begin work on password-store

---
+++ nixos/modules/packages/terminal-tools/password-store.nix
@@ -0,0 +1,18 @@
+{ config, lib, pkgs, ... }:
+{
+	options = {
+		passNixosModule.enable = lib.mkEnableOption "Configures some dependencies necessary to get password-store to work (in home manager)";
+	};
+
+	config = lib.mkIf config.passNixosModule.enable {
+    services.pcscd.enable = true;
+    programs.gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
+      pinentryPackage = pkgs.pinentry-curses;
+    };
+    environment.systemPackages = with pkgs; [
+      passff-host
+    ];
+	};
+}
