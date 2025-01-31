d9f5e13a49bbe7c07a700793e3f4debd452985c8
Fernando RF <ferife@icloud.com>
2025-01-31 09:24:53 -0600

    Uninstall pandoc until I get it to work

--- homeManager/modules/packages/terminal-apps/command-tools/default.nix
+++ homeManager/modules/packages/terminal-apps/command-tools/default.nix
@@ -17,8 +17,9 @@
   fzfModule.enable    = lib.mkDefault true;
   gitModule.enable    = lib.mkDefault true;
   lsdModule.enable    = lib.mkDefault true;
-  pandocModule.enable = lib.mkDefault true;
   passModule.enable   = lib.mkDefault true;
   tldrModule.enable   = lib.mkDefault true;
   zoxideModule.enable = lib.mkDefault true;
+
+  pandocModule.enable = lib.mkDefault false;
 }
--- homeManager/modules/packages/terminal-apps/command-tools/pandoc.nix
+++ homeManager/modules/packages/terminal-apps/command-tools/pandoc.nix
@@ -7,10 +7,13 @@
 	config = lib.mkIf config.pandocModule.enable {
     programs.texlive = {  # texlive packages are a pandoc dependency
       enable = true;
-      
+      packageSet = pkgs.texlive.combined.scheme-small;
     };
 		programs.pandoc = {
       enable = true;
     };
 	};
 }
+
+# FIXME: Figure out how to use texlive to install pdflatex, a dependency of pandoc
+# pdflatex is not available by itself in nixpkgs, which is why I need to figure out how to use texlive for this instead
