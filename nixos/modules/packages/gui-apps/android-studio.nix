3cfd3b7a0b42cc372d476082475ee8f7e8b7cc0d
Fernando RF <ferife@icloud.com>
2025-03-05 05:41:24 -0600

    Move Android Studio installation to nixpkgs unstable branch
    Also added user stuff for hardware accelerated android emulation

--- nixos/modules/packages/gui-apps/android-studio.nix
+++ nixos/modules/packages/gui-apps/android-studio.nix
@@ -1,13 +1,14 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs-unstable, ... }:
 {
 	options = {
 		androidStudio.enable = lib.mkEnableOption "Android Studio, an IDE designed by Google specifically to create Android applications";
 	};
 
 	config = lib.mkIf config.androidStudio.enable {
-		environment.systemPackages = with pkgs; [
+		environment.systemPackages = with pkgs-unstable; [
       android-studio
     ];
     nixpkgs.config.android_sdk.accept_license = true;
+    users.users.fernandorf.extraGroups = [ "kvm" ];
 	};
 }
