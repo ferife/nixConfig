a6ba4b03fc275bbfdbc45cd0f866ee41cb217cc9
Fernando RF <ferife@icloud.com>
2025-03-07 07:11:54 -0600

    Add lazygit shell aliases
    Also add note about how to git reset in lazygit

--- homeManager/modules/packages/terminal-apps/tui-apps/lazygit.nix
+++ homeManager/modules/packages/terminal-apps/tui-apps/lazygit.nix
@@ -8,5 +8,12 @@
     programs.lazygit = {
       enable = true;
     };
+    home.shellAliases = {
+      "lg" = "lazygit";
+      "lazy" = "lazygit";
+    };
   };
 }
+# NOTE: To undo commits in lazygit and put their changes back in the working tree, do the following:
+# Go to the commits window and put the cursor on the commit that will be the new head
+# Press "g" (git reset) and select the "mixed" option
