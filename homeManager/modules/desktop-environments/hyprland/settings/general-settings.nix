872a58d3bb329b4af67452637759d2d35e619e99
Fernando RF <ferife@icloud.com>
2025-03-19 09:43:32 -0500

    Begin working on Hyprland

---
+++ homeManager/modules/desktop-environments/hyprland/settings/general-settings.nix
@@ -0,0 +1,44 @@
+{ config, lib, pkgs, inputs, ... }:
+let
+  # startupScript = pkgs.pkgs.writeShellScriptBin "start" ''
+  #   ${pkgs.waybar}/bin/waybar &
+  # '';
+in {
+  options = {
+    hyprland.generalSettings.enable = lib.mkEnableOption "Enable some basic settings for GNOME";
+  };
+
+  config = lib.mkIf config.hyprland.generalSettings.enable {
+
+    home.sessionVariables.NIXOS_OZONE_WL = "1";
+    # hint Electron apps to use Wayland
+
+    wayland.windowManager.hyprland = {
+      # allow home manager to configure hyprland
+      enable = true;
+
+      # plugins = [
+      #   inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.borders-plus-plus
+      # ];
+
+      settings = {
+
+        "plugin:borders-plus-plus" = {
+          add_borders = 1;
+
+          "col.border_1" = "rgb(ffffff)";
+          "col.border_2" = "rgb(2222ff)";
+        };
+        border_size_1 = 10;
+        border_size_2 = -1;
+
+        natural_rounding = "yes";
+      };
+    };
+
+    programs.waybar = {
+      enable = true;
+      # settings = {};
+    };
+  };
+}
---
+++ nixos/modules/desktop-environments/hyprland/general-settings.nix
@@ -0,0 +1,20 @@
+{ config, lib, pkgs, inputs, ... }:
+{
+	options = {
+		hyprland.settings.enable = lib.mkEnableOption "Basic NixOS config for Hyprland that needed to happen outside of Home Manager";
+	};
+
+	config = lib.mkIf config.hyprland.settings.enable {
+    # nix.settings = {
+    #   substituters = ["https://hyprland.cachix.org"];
+    #   trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    # };
+    programs.hyprland = {
+      enable = true;
+      package = inputs.hyprland.packages."${pkgs.system}".hyprland;
+    };
+    environment.variables = {
+      NIXOS_OZONE_WL = "1";
+    };
+  };
+}
