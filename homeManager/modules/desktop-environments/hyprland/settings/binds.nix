e262e7705534230f9f2ce919d028a6f9da9d00d4
Fernando RF <ferife@icloud.com>
2025-03-20 14:27:31 -0500

    Move binds to their own file

---
+++ homeManager/modules/desktop-environments/hyprland/settings/binds.nix
@@ -0,0 +1,24 @@
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}: {
+  config = lib.mkIf config.hm.hyprland.enable {
+    wayland.windowManager.hyprland.settings = lib.mkMerge [
+      # Format for a bindd with questions:
+      # "MODS, key, description, dispatcher, params"
+      # TODO: make the specific terminal and browser depend on userSettings
+      (lib.mkIf config.hm.kitty {
+        bindd = ["$mainMod, T, Open a terminal window, exec, kitty"];
+      })
+      (lib.mkIf config.hm.firefox {
+        bindd = ["$mainMod, B, Open a browser window, exec, firefox"];
+      })
+      (lib.mkIf config.hm.hyprland.wofi {
+        bindd = ["$mainMod, A, Open the app launcher, exec, wofi --show drun --allow-images"];
+      })
+    ];
+  };
+}
