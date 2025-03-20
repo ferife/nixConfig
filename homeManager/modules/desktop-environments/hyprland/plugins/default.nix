aba6e4f711a03b859514c462e8a6ed93c8bd5da8
Fernando RF <ferife@icloud.com>
2025-03-20 08:00:37 -0500

    Reorganize home-manager config

---
+++ homeManager/modules/desktop-environments/hyprland/plugins/default.nix
@@ -0,0 +1,12 @@
+{ config, lib, pkgs, ... }:
+{
+  imports = [
+    ./options.nix
+
+    ./waybar.nix
+  ];
+
+  config.hm.hyprland = {
+    waybar = lib.mkDefault config.hm.hyprland.enable;
+  };
+}
