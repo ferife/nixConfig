da5f3e2adbab347217a7193e9c886e8b3811b08a
Fernando RF <ferife@icloud.com>
2025-03-20 14:19:04 -0500

    Install hypridle

--- homeManager/modules/desktop-environments/hyprland/plugins/default.nix
+++ homeManager/modules/desktop-environments/hyprland/plugins/default.nix
@@ -7,11 +7,13 @@
   imports = [
     ./options.nix
 
+    ./hypridle.nix
     ./waybar.nix
     ./wofi.nix
   ];
 
   config.hm.hyprland = {
+    hypridle = lib.mkDefault config.hm.hyprland.enable;
     waybar = lib.mkDefault config.hm.hyprland.enable;
     wofi = lib.mkDefault config.hm.hyprland.enable;
   };
