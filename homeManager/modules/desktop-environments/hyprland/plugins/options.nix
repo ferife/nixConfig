3712339ff42c48c78a5885661e5dde974fb9d1de
Fernando RF <ferife@icloud.com>
2025-03-20 08:32:58 -0500

    Autoformat with alejandra

--- homeManager/modules/desktop-environments/hyprland/plugins/options.nix
+++ homeManager/modules/desktop-environments/hyprland/plugins/options.nix
@@ -1,5 +1,9 @@
-{ config, lib, pkgs, ... }:
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   options = {
     hm.hyprland.waybar = lib.mkEnableOption "Waybar, a highly customizable status bar for wayland window managers";
   };
