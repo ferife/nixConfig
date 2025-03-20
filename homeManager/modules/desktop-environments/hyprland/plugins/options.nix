81f09d9773b5fe83f5340c45210e60ba4925608d
Fernando RF <ferife@icloud.com>
2025-03-20 14:18:44 -0500

    Install wofi

--- homeManager/modules/desktop-environments/hyprland/plugins/options.nix
+++ homeManager/modules/desktop-environments/hyprland/plugins/options.nix
@@ -6,5 +6,6 @@
 }: {
   options = {
     hm.hyprland.waybar = lib.mkEnableOption "Waybar, a highly customizable status bar for wayland window managers";
+    hm.hyprland.wofi = lib.mkEnableOption "Wofi, an app launcher for wayland window managers";
   };
 }
