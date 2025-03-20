3eda7d90b4d649cc2e393659f4f13beeb8f69f91
Fernando RF <ferife@icloud.com>
2025-03-20 13:44:09 -0500

    Add todos

--- homeManager/modules/packages/terminal-apps/default.nix
+++ homeManager/modules/packages/terminal-apps/default.nix
@@ -10,3 +10,11 @@
     ./tui-apps
   ];
 }
+# TODO: Check out the following tools
+# coreutils - Basic helper apps
+# fd - replacement for find
+# jq - Tool for formatting/querying JSON
+# procs - ps replacement
+# ripgrep - grep replacement
+# zip
+
