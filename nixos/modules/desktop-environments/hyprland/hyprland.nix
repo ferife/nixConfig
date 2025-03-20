3a6371592896e94d164f40d7b7dbb3447c4c2d32
Fernando RF <ferife@icloud.com>
2025-03-20 07:57:32 -0500

    Enable xwayland

--- nixos/modules/desktop-environments/hyprland/hyprland.nix
+++ nixos/modules/desktop-environments/hyprland/hyprland.nix
@@ -7,6 +7,7 @@
     # };
     programs.hyprland = {
       enable = true;
+      xwayland.enable = true;
       # package = inputs.hyprland.packages."${pkgs.system}".hyprland;
     };
     environment.variables = {
