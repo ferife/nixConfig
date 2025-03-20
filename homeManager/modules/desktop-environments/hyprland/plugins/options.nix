8c33d9b38aab1fa94d155c8d581db0e3e0564999
Fernando RF <ferife@icloud.com>
2025-03-20 14:19:04 -0500

    Install hypridle

--- homeManager/modules/desktop-environments/hyprland/plugins/options.nix
+++ homeManager/modules/desktop-environments/hyprland/plugins/options.nix
@@ -5,6 +5,7 @@
   ...
 }: {
   options = {
+    hm.hyprland.hypridle = lib.mkEnableOption "Hypridle, Hyprland's official idle daemon, which handles what happens when the computer sits idle for an extended period of time";
     hm.hyprland.waybar = lib.mkEnableOption "Waybar, a highly customizable status bar for wayland window managers";
     hm.hyprland.wofi = lib.mkEnableOption "Wofi, an app launcher for wayland window managers";
   };
