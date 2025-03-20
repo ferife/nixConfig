dfafbbe7d92258da1e571c886c20b4428b6bcce7
Fernando RF <ferife@icloud.com>
2025-03-20 16:12:27 -0500

    Waybar - begin configuration

--- homeManager/modules/desktop-environments/hyprland/plugins/waybar.nix
+++ homeManager/modules/desktop-environments/hyprland/plugins/waybar.nix
@@ -8,10 +8,35 @@
   config = lib.mkIf config.hm.hyprland.waybar {
     programs.waybar = {
       enable = true;
-      # settings = {};
+      settings = {
+        mainBar = {
+          layer = "top";
+          position = "top";
+          # mod = "dock";
+
+          modules-left = [
+            # "symbols"
+            "clock"
+            # "custom/weather"
+            "hyprland/workspaces"
+          ];
+          modules-center = [
+            "hyprland/window"
+          ];
+          modules-right = [
+            "tray"
+            # "custom/language"
+            "battery"
+            # "backlight"
+            # "pulseaudio"
+            # "pulseaudio#microphone"
+          ];
+        };
+      };
     };
     home.packages = with pkgs; [
-      font-awesome # Dependency for displaying symbols in the bar
+      nerdfonts # Dependency for displaying symbols in the bar
+      # WARNING: nerdfonts alone takes up 8GB of storage
     ];
     wayland.windowManager.hyprland.settings.exec-once = ["waybar"];
   };
