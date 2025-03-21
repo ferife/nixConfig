43efe9f6b2c7b650a3c4ed6da3222700d9e0503a
Fernando RF <ferife@icloud.com>
2025-03-21 12:15:22 -0500

    Waybar - basic styling
    a

--- homeManager/modules/desktop-environments/hyprland/plugins/waybar.nix
+++ homeManager/modules/desktop-environments/hyprland/plugins/waybar.nix
@@ -27,12 +27,12 @@
 
           modules-left = [
             # "symbols"
-            "clock"
             # "custom/weather"
             "hyprland/workspaces"
+            "hyprland/window"
           ];
           modules-center = [
-            "hyprland/window"
+            "clock"
           ];
           modules-right = [
             "tray"
@@ -44,6 +44,18 @@
           ];
         };
       };
+
+      # TODO: Add weather to waybar
+      # TODO: Add power menu to waybar (power off, reboot, sleep)
+      # TODO: Configure & style the workspaces, add a button to generate a workspace
+      # TODO: Add a widget for the current status of my VPN
+      # TODO: Add CPU usage, RAM usage, upload and download speed
+      # TODO: Add general menu with the following options
+      #   App launcher
+      #   Keyboard state (caps lock)
+      #   An idle inhibitor (like what I have in GNOME with Forge)
+
+      style = ''${builtins.readFile ./waybar.css}'';
     };
 
     home.packages = with pkgs; [
