c09fb498e080b0f03285edc247d2cfa1c5345eef
Fernando RF <ferife@icloud.com>
2025-03-31 11:27:16 -0500

    Allow the firefox/floorp extension to work

--- homeManager/modules/packages/terminal-apps/command-tools/password-store.nix
+++ homeManager/modules/packages/terminal-apps/command-tools/password-store.nix
@@ -8,16 +8,17 @@
     programs.password-store = {
       enable = true;
     };
-    home.packages = with pkgs; [
-      passff-host # Required for the firefox extension to work
-      python313 # Required for the firefox extension to work
-    ];
+
     # Some extra dependency stuff in nixos/modules/packages/cli-tools/password-store.nix
+
+    home.file.passff-host-workaround = {
+      # WARN: This is a dirty workaround done until Home Manager maintainers fix the issue
+      target = "${config.home.homeDirectory}/.mozilla/native-messaging-hosts/passff.json";
+      source = "${pkgs.passff-host}/share/passff-host/passff.json";
+    };
   };
+  # TODO: Cancel ExpressVPN subscription
+  # TODO: Set up pass with ios app
+  # TODO: Also set up pass-otp (github:tadfisher/pass-otp)
+  # The zbarimg is a command tool that can read images for QR codes and spit out the URLs they lead to
 }
-# TODO: Cancel ExpressVPN subscription
-# TODO: Set up pass with firefox extension and ios app
-
-# TODO: Also set up pass-otp (github:tadfisher/pass-otp)
-
-# The zbarimg is a command tool that can read images for QR codes and spit out the URLs they lead to
