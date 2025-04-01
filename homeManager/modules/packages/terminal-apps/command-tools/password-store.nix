a99758b71dfcf82805bb478c50e4b515473c3946
Fernando RF <ferife@icloud.com>
2025-04-01 05:06:27 -0500

    Squash merge password-store into main
    Modify TODOs
    
    Allow the firefox/floorp extension to work
    
    Remove comment about dependencies
    
    Install applications used for setting Pass for iOS
    
    Finish setup for Pass for iOS
    
    Auto pull from remote repo

--- homeManager/modules/packages/terminal-apps/command-tools/password-store.nix
+++ homeManager/modules/packages/terminal-apps/command-tools/password-store.nix
@@ -8,13 +8,24 @@
     programs.password-store = {
       enable = true;
     };
-    home.packages = with pkgs; [
-      passff-host # Required for the firefox extension to work
-      python313 # Required for the firefox extension to work
-    ];
-    # Some extra dependency stuff in nixos/modules/packages/cli-tools/password-store.nix
-  };
-}
-# FIXME: password-store
-# TODO: Cancel ExpressVPN subscription
 
+    home.file.passff-host-workaround = {
+      # WARN: This is a dirty workaround done until Home Manager maintainers fix the issue
+      target = "${config.home.homeDirectory}/.mozilla/native-messaging-hosts/passff.json";
+      source = "${pkgs.passff-host}/share/passff-host/passff.json";
+    };
+
+    # NOTE: The following packages are only really necessary for setting up password-store for ios and firefox. Once that's done, I don't need them anymore
+    # home.packages = with pkgs; [
+    #   qrencode # Library for encoding data into a QR code, used to transfer public & private keys safely
+    #   imagemagick # Image file manipulation, used here to convert PNGs to GIF
+    # ];
+
+    # Sync passwords whenever shell gets (re)initialized
+    programs.bash.initExtra = lib.mkIf (config.hm.bash) "pass git pull origin main";
+    programs.zsh.initExtra = lib.mkIf (config.hm.zsh) "pass git pull origin main";
+  };
+  # TODO: Cancel ExpressVPN subscription
+  # TODO: Also set up pass-otp (github:tadfisher/pass-otp)
+  # NOTE: To sync this with the iphone app, I need to set the URL for the git repo to `ssh://git@github.com/<username>/<repo name>.git`, set the branch name, set the username to `git`, and use the SSH key to log in
+}
