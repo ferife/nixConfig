b765dc7e74e5b2581ace339e7619569ea909f40d
Fernando RF <ferife@icloud.com>
2025-04-28 11:55:17 -0500

    Reformat kitty.nix and add themeFile

--- homeManager/modules/packages/terminal-apps/terminal-emulators/kitty.nix
+++ homeManager/modules/packages/terminal-apps/terminal-emulators/kitty.nix
@@ -1,19 +1,30 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: {
-  config = lib.mkIf config.hm.kitty {
-    programs.kitty = lib.mkMerge [
-      {
-        enable = true;
-        settings.background_opacity = lib.mkForce 0.9;
-      }
+  config = lib.mkMerge [
+    (lib.mkIf config.hm.kitty {programs.kitty.enable = true;})
 
-      (lib.mkIf config.hm.bash {shellIntegration.enableBashIntegration = true;})
-      (lib.mkIf config.hm.zsh {shellIntegration.enableZshIntegration = true;})
-    ];
-  };
-  # TODO: Customize font
+    (lib.mkIf (config.hm.stylix.theme == "onedark") {
+      stylix.targets.kitty.enable = false;
+      programs.kitty.themeFile = "OneDark";
+    })
+
+    {
+      programs.kitty = {
+        settings.background_opacity = lib.mkForce 0.9;
+        font = lib.mkForce {
+          name = "Fira Code";
+          package = pkgs.fira-code;
+          size = 12;
+        };
+      };
+    }
+
+    (lib.mkIf config.hm.bash {programs.kitty.shellIntegration.enableBashIntegration = true;})
+    (lib.mkIf config.hm.zsh {programs.kitty.shellIntegration.enableZshIntegration = true;})
+  ];
   # NOTE: To search through command history in the terminal using stylix, simply use CTRL+R
 }
