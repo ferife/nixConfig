0577adb42d898d328ad474694568f2b0ad40dceb
Fernando RF <ferife@icloud.com>
2025-04-29 11:37:59 -0500

    Re-enable Gaming

--- nixos/hosts/laptop/configuration.nix
+++ nixos/hosts/laptop/configuration.nix
@@ -19,5 +19,5 @@
   networking.hostName = systemSettings.hostname1;
   # Should match the name given in the NixOS configuration entrypoint in flake.nix
 
-  nixos.gaming.enable = lib.mkForce false; # Temporarily, as I finish the semester
+  # nixos.gaming.enable = lib.mkForce false; # Temporarily, as I finish the semester
 }
