2b3c10e739b5b2ec852a9293bad4352c67ab6232
Fernando RF <ferife@icloud.com>
2025-05-02 09:21:03 -0500

    Modify TODOs

--- nixos/modules/settings/low-power-options.nix
+++ nixos/modules/settings/low-power-options.nix
@@ -14,8 +14,5 @@
 
       criticalPowerAction = "Hibernate";
     };
-    # TODO: Set up actions for the computer to use when the battery is running low
-    # Use the NIXOS Options that exist for services.upower
-    # Ensure that these action take into account whether the battery is charging or not
   };
 }
