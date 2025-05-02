c06191f119db771a2cd6b0dd68b38f8210c895f0
Fernando RF <ferife@icloud.com>
2025-05-02 12:06:15 -0500

    Rename mullvad-vpn.nix -> mullvad.nix and reformat

--- nixos/modules/packages/gui-apps/mullvad.nix
+++ nixos/modules/packages/gui-apps/mullvad.nix
@@ -9,11 +9,9 @@
       mullvad # CLI
       mullvad-vpn # GUI
     ];
-    networking.firewall.checkReversePath = "loose";
     networking.wireguard.enable = true;
-    services.mullvad-vpn = {
-      enable = true;
-    };
+    services.mullvad-vpn.enable = true;
+    # networking.firewall.checkReversePath = "loose"; # Doen automatically by services.mullvad-vpn.enable = true;
   };
   # TODO: Set a shell script that will auto log me in to my Mullvad account
   # REMEMBER that the account number should be set up as a secret
