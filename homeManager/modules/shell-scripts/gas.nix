47ffbabd286362fe2c3122cace26a69e84f366d7
Fernando RF <ferife@icloud.com>
2025-05-02 08:05:00 -0500

    Separate declaration of script from bin

--- homeManager/modules/shell-scripts/gas.nix
+++ homeManager/modules/shell-scripts/gas.nix
@@ -4,15 +4,16 @@
   pkgs,
   ...
 }: let
-  new-gas = pkgs.writeShellScriptBin "newGas" ''
+  new-gas-script = ''
     pathAlejandra=$(which alejandra 2> /dev/null) || pathAlejandra=${pkgs.alejandra}/bin/alejandra
     echo "$pathAlejandra"
     echo "Home Manager Script"
   '';
+  new-gas-bin = pkgs.writeShellScriptBin "newGas" new-gas-script;
 in {
   config = lib.mkIf config.hm.scripts.gas {
     home.packages = [
-      new-gas
+      new-gas-bin
     ];
   };
 }
