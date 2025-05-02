329cb55331ce14ca77b78e4473b54b05c9a3ecea
Fernando RF <ferife@icloud.com>
2025-05-02 07:37:10 -0500

    Improve gas.nix

--- nixos/modules/shell-scripts/gas.nix
+++ nixos/modules/shell-scripts/gas.nix
@@ -5,11 +5,13 @@
   ...
 }: let
   new-gas = pkgs.writeShellScriptBin "newGas" ''
-    echo "Gas Script"
+    pathAlejandra=$(which alejandra 2> /dev/null) || pathAlejandra=${pkgs.alejandra}/bin/alejandra
+    echo "$pathAlejandra"
+    echo "NixOS Script"
   '';
 in {
   config = lib.mkIf config.nixos.scripts.gas {
-    environment.systemPackages = with pkgs; [
+    environment.systemPackages = [
       new-gas
     ];
   };
