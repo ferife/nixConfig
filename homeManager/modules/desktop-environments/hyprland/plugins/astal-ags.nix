8b14597efab6f96c36e6c18c50b64586013abfa0
Fernando RF <ferife@icloud.com>
2025-05-12 06:28:46 -0500

    Remove ags and install astal directly
    Using ags would require setting up a traditional JS project, which means
    using npm with a node_modules folder
    
    I don't wish to mix package managers within my nix configuration
    
    I will not install any package managers other than nix unless it is
    required for a specific project I'm working on

--- homeManager/modules/desktop-environments/hyprland/plugins/astal-ags.nix
+++ homeManager/modules/desktop-environments/hyprland/plugins/astal-ags.nix
@@ -3,21 +3,10 @@
   lib,
   pkgs,
   inputs,
+  system,
   ...
 }: {
-  imports = [inputs.ags.homeManagerModules.default];
   config = lib.mkIf config.hm.hyprland.astal {
-    programs.ags = {
-      enable = true;
-      configDir = null;
-      extraPackages = with pkgs; [
-        inputs.ags.packages.${pkgs.system}.battery
-        fzf
-      ];
-    };
-    home.packages = with inputs.ags.packages.${pkgs.system}; [
-      io
-      notifd
-    ];
+    home.packages = [inputs.astal.packages.${system}.default];
   };
 }
