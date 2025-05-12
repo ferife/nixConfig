bb5489e00f6ca12536c05975d02cd2bef652ff9b
Fernando RF <ferife@icloud.com>
2025-05-12 05:56:15 -0500

    TEMP Install brightnessctl

--- nixos/modules/desktop-environments/hyprland/default.nix
+++ nixos/modules/desktop-environments/hyprland/default.nix
@@ -11,7 +11,8 @@
   ];
 
   nixos.hyprland.enable = lib.mkDefault true;
+
   # TODO: Use ONE of the two following lines to install a program to control screen light
   # programs.light.enable = true;
-  # environment.systemPackages = [pkgs.brightnessctl];
+  environment.systemPackages = [pkgs.brightnessctl];
 }
