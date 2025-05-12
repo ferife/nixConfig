1c08cf724da02b69ae8fd2629b3a43a1e1e3c34c
Fernando RF <ferife@icloud.com>
2025-05-12 04:43:32 -0500

    Add todo

--- nixos/modules/desktop-environments/hyprland/default.nix
+++ nixos/modules/desktop-environments/hyprland/default.nix
@@ -11,4 +11,7 @@
   ];
 
   nixos.hyprland.enable = lib.mkDefault true;
+  # TODO: Use ONE of the two following lines to install a program to control screen light
+  # programs.light.enable = true;
+  # environment.systemPackages = [pkgs.brightnessctl];
 }
