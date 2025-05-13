b5558c27dc3215ad6c6e4bbe3b5f3570f13f1dea
Fernando RF <ferife@icloud.com>
2025-05-13 04:30:16 -0500

    Modularize nixos/general-config.nix

--- nixos/general-config.nix
+++ nixos/general-config.nix
@@ -11,111 +11,165 @@
 }: {
   imports = [./minimal-config.nix];
 
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  config = lib.mkMerge [
+    {
+      # Networking
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+      # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Enable networking
-  networking.networkmanager.enable = true;
+      # Configure network proxy if necessary
+      # networking.proxy.default = "http://user:password@proxy:port/";
+      # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Set your time zone.
-  time.timeZone = "${systemSettings.timezone}";
+      # Enable networking
+      networking.networkmanager.enable = true;
+    }
+    {
+      # Location information
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "${systemSettings.locale}";
+      # Set your time zone.
+      time.timeZone = "${systemSettings.timezone}";
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "${systemSettings.locale}";
-    LC_IDENTIFICATION = "${systemSettings.locale}";
-    LC_MEASUREMENT = "${systemSettings.locale}";
-    LC_MONETARY = "${systemSettings.locale}";
-    LC_NAME = "${systemSettings.locale}";
-    LC_NUMERIC = "${systemSettings.locale}";
-    LC_PAPER = "${systemSettings.locale}";
-    LC_TELEPHONE = "${systemSettings.locale}";
-    LC_TIME = "${systemSettings.locale}";
-  };
+      # Select internationalisation properties.
+      i18n.defaultLocale = "${systemSettings.locale}";
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
+      i18n.extraLocaleSettings = {
+        LC_ADDRESS = "${systemSettings.locale}";
+        LC_IDENTIFICATION = "${systemSettings.locale}";
+        LC_MEASUREMENT = "${systemSettings.locale}";
+        LC_MONETARY = "${systemSettings.locale}";
+        LC_NAME = "${systemSettings.locale}";
+        LC_NUMERIC = "${systemSettings.locale}";
+        LC_PAPER = "${systemSettings.locale}";
+        LC_TELEPHONE = "${systemSettings.locale}";
+        LC_TIME = "${systemSettings.locale}";
+      };
+    }
+    {
+      # X11
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
+      # Enable the X11 windowing system.
+      services.xserver.enable = true;
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
+      # Configure keymap in X11
+      services.xserver.xkb = {
+        layout = "us";
+        variant = "";
+      };
+    }
+    (lib.mkIf (systemSettings.nixpkgs == "unstable") {
+      # IO Device settings
 
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+      # Enable CUPS to print documents.
+      # services.printing.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
+      # Enable sound with pipewire.
+      services.pulseaudio.enable = false;
+      security.rtkit.enable = true;
+      services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        # If you want to use JACK applications, uncomment this
+        #jack.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+        # use the example session manager (no others are packaged yet so this is enabled by default,
+        # no need to redefine it in your config for now)
+        #media-session.enable = true;
+      };
 
-  # Enable flakes
-  nix.settings.experimental-features = ["nix-command" "flakes"];
-  nixpkgs.config.allowUnfree = true;
+      # Enable touchpad support (enabled default in most desktopManager).
+      # services.xserver.libinput.enable = true;
+    })
+    (lib.mkIf (systemSettings.nixpkgs == "stable") {
+      # IO Device settings
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+      # Enable CUPS to print documents.
+      # services.printing.enable = true;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+      # Enable sound with pipewire.
+      hardware.pulseaudio.enable = false;
+      security.rtkit.enable = true;
+      services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        # If you want to use JACK applications, uncomment this
+        #jack.enable = true;
 
-  # List services that you want to enable:
+        # use the example session manager (no others are packaged yet so this is enabled by default,
+        # no need to redefine it in your config for now)
+        #media-session.enable = true;
+      };
 
-  # Enable the OpenSSH daemon.
-  # This would allow me to ssh into this computer from any other device
-  # services.openssh.enable = true;
+      # Enable touchpad support (enabled default in most desktopManager).
+      # services.xserver.libinput.enable = true;
+    })
+    {
+      # Flakes & nixpkgs
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+      nix.settings.experimental-features = ["nix-command" "flakes"];
+      nixpkgs.config.allowUnfree = true;
+    }
+    {
+      # SUID Wrappers
 
-  # Enable software for updating firmware
-  services.fwupd.enable = true;
+      # Some programs need SUID wrappers, can be configured further or are
+      # started in user sessions.
+      # programs.mtr.enable = true;
+      # programs.gnupg.agent = {
+      #   enable = true;
+      #   enableSSHSupport = true;
+      # };
+    }
+    {
+      # SSH and Firewalls
 
-  # The following installs documentation for all existing nixos options
-  documentation.man.enable = true;
-  documentation.doc.enable = true;
-  documentation.nixos = {
-    enable = true;
+      # List services that you want to enable:
 
-    # The following will cause the docs to include all custom options created within my config
-    extraModuleSources = [
-      ./modules
-    ];
-  };
-  # FIXME: The above will cause errors with stylix
-  # Ensure that ONLY MY documentation is generated, not the docs for stylix or anything else
+      # Enable the OpenSSH daemon.
+      # This would allow me to ssh into this computer from any other device
+      # services.openssh.enable = true;
 
-  # The following lines enable certain programs that I consider essential for the minimum installation of a computer using this config
-  nixos.git = lib.mkForce true;
-  nixos.fastfetch = lib.mkForce true;
-  nixos.homeManager = lib.mkForce true;
-  nixos.nh.enable = lib.mkForce true;
+      # Open ports in the firewall.
+      # networking.firewall.allowedTCPPorts = [ ... ];
+      # networking.firewall.allowedUDPPorts = [ ... ];
+      # Or disable the firewall altogether.
+      # networking.firewall.enable = false;
+    }
+    {
+      # Firmware
+
+      # Enable software for updating firmware
+      services.fwupd.enable = true;
+    }
+    {
+      # Documentation
+
+      # The following installs documentation for all existing nixos options
+      documentation.man.enable = true;
+      documentation.doc.enable = true;
+      documentation.nixos = {
+        enable = true;
+
+        # The following will cause the docs to include all custom options created within my config
+        extraModuleSources = [
+          ./modules
+        ];
+      };
+      # FIXME: The above will cause errors with stylix
+      # Ensure that ONLY MY documentation is generated, not the docs for stylix or anything else
+    }
+    {
+      # Force install programs
+
+      # The following lines enable certain programs that I consider essential for the minimum installation of a computer using this config
+      nixos.git = lib.mkForce true;
+      nixos.fastfetch = lib.mkForce true;
+      nixos.homeManager = lib.mkForce true;
+      nixos.nh.enable = lib.mkForce true;
+    }
+  ];
 }
---
+++ temp.md
@@ -0,0 +1,16 @@
+hyprland wiki page on status bars
+nixvim setup for python development
+github:OfflineBot/nixos (someone's nix config with hyprland and ags)
+https://www.reddit.com/r/NixOS/comments/pomqf4/ricing_with_nixos/
+https://www.reddit.com/r/NixOS/comments/1eqf7jo/how_to_make_a_bar_the_nix_way/
+Astal docs
+https://wiki.nixos.org/wiki/Backlight#brightnessctl
+https://aylur.github.io/astal/guide/typescript/examples
+
+vim cheat sheet (looking for a nice graphic)
+hey.com (recommended by No Boilerplate)
+
+AppProg final exam
+AppProg download all presentations
+
+
