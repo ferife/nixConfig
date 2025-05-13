e1c6e6d1c92434957ac877b123c2c9ed14fce2cc
Fernando RF <ferife@icloud.com>
2025-05-13 13:01:56 -0500

    FIX: flake.nix and flake.lock

--- flake.nix
+++ flake.nix
@@ -20,6 +20,13 @@
     nixvim-config = {
       url = "github:ferife/nvimConfig";
     };
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
+    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+      inputs.hyprland.follows = "hyprland";
+    };
   };
   # NOTE: To update a single input, use the command `nix flake update <name-of-input>`
 
