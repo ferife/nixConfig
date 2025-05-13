a0e5d5bfa8008c5dbf569ef3c24aeb2b944984a2
Fernando RF <ferife@icloud.com>
2025-05-13 13:22:45 -0500

    FIX: flake.lock

--- nixos/modules/desktop-environments/hyprland/default.nix
+++ nixos/modules/desktop-environments/hyprland/default.nix
@@ -10,12 +10,9 @@
     ./hyprland.nix
   ];
 
-  # TODO: Set up all F-key functions
-
   config.nixos.hyprland.enable = lib.mkDefault true;
-  nixos.hyprland.enable = lib.mkDefault true;
 
   # TODO: Use ONE of the two following lines to install a program to control screen light
   # programs.light.enable = true;
-  environment.systemPackages = [pkgs.brightnessctl];
+  config.environment.systemPackages = [pkgs.brightnessctl];
 }
