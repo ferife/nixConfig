55f5777a49d49db4c804b1c4a984cc71082004cc
Fernando RF <ferife@icloud.com>
2025-05-30 12:11:28 -0500

    Add functionality to brightness f-keys

--- homeManager/modules/desktop-environments/hyprland/settings/binds/f-keys.nix
+++ homeManager/modules/desktop-environments/hyprland/settings/binds/f-keys.nix
@@ -6,6 +6,24 @@
   ...
 }: {
   config = lib.mkIf config.hm.hyprland.enable {
+    home.packages = [
+      pkgs.brightnessctl
+      pkgs.bc
+    ];
+
+    home.file = {
+      ".nix-scripts/modify-monitor-brightness.bash" = {
+        enable = true;
+        source = ./shell-scripts/monitor-brightness.bash;
+        executable = true;
+      };
+      ".nix-scripts/brightness-notification.bash" = {
+        enable = true;
+        source = ./shell-scripts/brightness-notification.bash;
+        executable = true;
+      };
+    };
+
     wayland.windowManager.hyprland.settings = lib.mkMerge [
       # Format for a bindd with questions:
       # "MODS, key, description, dispatcher, params"
@@ -23,6 +41,22 @@
 
           # F3
           ", XF86AudioLowerVolume, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ 0 && wpctl set-volume -l ${toString (config.hm.hyprland.max-volume / 100.0)} @DEFAULT_AUDIO_SINK@ 5%-"
+        ];
+      }
+      {
+        binde = [
+          # F7
+          ", XF86MonBrightnessUp, exec, ~/.nix-scripts/modify-monitor-brightness.bash intel_backlight 10%+"
+
+          # F8
+          ", XF86MonBrightnessDown, exec, ~/.nix-scripts/modify-monitor-brightness.bash intel_backlight 10%-"
+        ];
+        bindr = [
+          # F7
+          ", XF86MonBrightnessUp, exec, ~/.nix-scripts/brightness-notification.bash intel_backlight"
+
+          # F8
+          ", XF86MonBrightnessDown, exec, ~/.nix-scripts/brightness-notification.bash intel_backlight"
         ];
       }
     ];
