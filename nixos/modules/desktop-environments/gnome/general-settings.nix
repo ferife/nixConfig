b951255fae3c981dcdf3d77af9b6992fb26e6bd8
Fernando RF <ferife@icloud.com>
2025-06-13 06:44:06 -0500

    Change options to new names

--- nixos/modules/desktop-environments/gnome/general-settings.nix
+++ nixos/modules/desktop-environments/gnome/general-settings.nix
@@ -6,8 +6,8 @@
 }: {
   config = lib.mkIf config.nixos.gnome.enable {
     # Enable the GNOME Desktop Environment.
-    services.xserver.displayManager.gdm.enable = true;
-    services.xserver.desktopManager.gnome.enable = true;
+    services.displayManager.gdm.enable = true;
+    services.desktopManager.gnome.enable = true;
 
     services.gnome.gnome-browser-connector.enable = true; # Allows for GNOME extensions to affect browsers
   };
