03bead77b0cfdfa6c239da7ac3af424c13b6dbb3
Fernando RF <ferife@icloud.com>
2025-07-31 06:31:05 -0500

    Make file with hyprland f-key binds more readable

--- homeManager/modules/desktop-environments/window-managers/hyprland/settings/binds/f-keys.nix
+++ homeManager/modules/desktop-environments/window-managers/hyprland/settings/binds/f-keys.nix
@@ -11,33 +11,38 @@
 
     # The names of the F-keys (based on function, not number) can be seen at https://github.com/xkbcommon/libxkbcommon/blob/master/include/xkbcommon/xkbcommon-keysyms.h
 
-    wayland.windowManager.hyprland.settings = lib.mkMerge [
-      # Format for a bindd with questions:
-      # "MODS, key, description, dispatcher, params"
-      {
-        bind = [
-          # F1
-          ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle" #F1
-        ];
-      }
-      {
-        binde = [
-          # F2
-          ", XF86AudioRaiseVolume, exec, ${config.hm.scripts.modify-volume.full-path} @DEFAULT_AUDIO_SINK@ ${toString config.hm.wm.max-volume} 5%+"
+    wayland.windowManager.hyprland.settings = let
+      max-volume = toString config.hm.wm.max-volume;
+      modify-monitor-brightness = config.hm.scripts.modify-monitor-brightness.full-path;
+      modify-volume = config.hm.scripts.modify-volume.full-path;
+    in
+      lib.mkMerge [
+        # Format for a bindd with questions:
+        # "MODS, key, description, dispatcher, params"
+        {
+          bind = [
+            # F1
+            ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle" #F1
+          ];
+        }
+        {
+          binde = [
+            # F2
+            ", XF86AudioRaiseVolume, exec, ${modify-volume} @DEFAULT_AUDIO_SINK@ ${max-volume} 5%+"
 
-          # F3
-          ", XF86AudioLowerVolume, exec, ${config.hm.scripts.modify-volume.full-path} @DEFAULT_AUDIO_SINK@ ${toString config.hm.wm.max-volume} 5%-"
-        ];
-      }
-      {
-        binde = [
-          # F7
-          ", XF86MonBrightnessUp, exec, ${config.hm.scripts.modify-monitor-brightness.full-path} intel_backlight 5%+"
+            # F3
+            ", XF86AudioLowerVolume, exec, ${modify-volume} @DEFAULT_AUDIO_SINK@ ${max-volume} 5%-"
+          ];
+        }
+        {
+          binde = [
+            # F7
+            ", XF86MonBrightnessUp, exec, ${modify-monitor-brightness} intel_backlight 5%+"
 
-          # F8
-          ", XF86MonBrightnessDown, exec, ${config.hm.scripts.modify-monitor-brightness.full-path} intel_backlight 5%-"
-        ];
-      }
-    ];
+            # F8
+            ", XF86MonBrightnessDown, exec, ${modify-monitor-brightness} intel_backlight 5%-"
+          ];
+        }
+      ];
   };
 }
