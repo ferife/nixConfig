9230ac6a4330d29fa4f9d71015027773c7170dcb
Fernando RF <ferife@icloud.com>
2025-08-09 12:08:14 -0500

    Enable bluetoth

--- nixos/modules/settings/options.nix
+++ nixos/modules/settings/options.nix
@@ -4,6 +4,7 @@
   ...
 }: {
   options.nixos = {
+    bluetooth = lib.mkEnableOption "bluetooth";
     documentation = lib.mkEnableOption "autogeneration of documentation for this config";
     firmware-updater = lib.mkEnableOption "a firmware updater";
     force-install-basics = lib.mkEnableOption "the forced installation of certain basic programs required for this config to work";
