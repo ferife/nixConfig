a055885f42ac1773616af1538f556335254296de
Fernando RF <ferife@icloud.com>
2025-08-15 05:28:00 -0500

    Minor refactor of homeManager/.../stylix.nix

--- homeManager/modules/appearance/stylix.nix
+++ homeManager/modules/appearance/stylix.nix
@@ -6,35 +6,40 @@
 }: {
   config = with lib;
     mkIf config.hm.stylix.enable {
-      stylix = with lib;
-        mkMerge [
-          # General Settings
-          {
-            enable = true;
-            autoEnable = true;
-            # fonts = {
-            #   sansSerif = {
-            #     package = pkgs.fira;
-            #     name = "Fira Sans";
-            #   };
-            #   serif = config.stylix.fonts.sansSerif;
-            #   monospace = {
-            #     package = pkgs.nerd-fonts.fira-code;
-            #     name = "Fira Code";
-            #   };
-            #   emoji = {
-            #     package = pkgs.noto-fonts-monochrome-emoji;
-            #     name = "Noto Emoji";
-            #   };
-            # };
-          }
+      stylix = lib.mkMerge [
+        # General Settings
+        {
+          enable = true;
+          autoEnable = true;
+          # fonts = {
+          #   monospace = {
+          #     package = pkgs.nerd-fonts.fira-code;
+          #     name = "Fira Code Nerdfont";
+          #   };
+          # };
+          # fonts = {
+          #   sansSerif = {
+          #     package = pkgs.fira;
+          #     name = "Fira Sans";
+          #   };
+          #   serif = config.stylix.fonts.sansSerif;
+          #   monospace = {
+          #     package = pkgs.nerd-fonts.fira-code;
+          #     name = "Fira Code";
+          #   };
+          #   emoji = {
+          #     package = pkgs.noto-fonts-monochrome-emoji;
+          #     name = "Noto Emoji";
+          #   };
+          # };
+        }
 
-          # Onedark
-          (mkIf (config.hm.stylix.theme == "onedark") {
-            image = ../../../generalResources/background-images/onedark/od_nixos_logo.png;
-            base16Scheme = "${pkgs.base16-schemes}/share/themes/onedark.yaml";
-            polarity = "dark";
-          })
-        ];
+        # Onedark
+        (mkIf (config.hm.stylix.theme == "onedark") {
+          image = ../../../generalResources/background-images/onedark/od_nixos_logo.png;
+          base16Scheme = "${pkgs.base16-schemes}/share/themes/onedark.yaml";
+          polarity = "dark";
+        })
+      ];
     };
 }
