f1bcd637891f3f18123e8a45de11eee01678ab79
Fernando RF <ferife@icloud.com>
2025-08-15 04:01:27 -0500

    Update power config options

--- nixos/modules/settings/options.nix
+++ nixos/modules/settings/options.nix
@@ -8,6 +8,6 @@
     documentation = lib.mkEnableOption "autogeneration of documentation for this config";
     firmware-updater = lib.mkEnableOption "a firmware updater";
     force-install-basics = lib.mkEnableOption "the forced installation of certain basic programs required for this config to work";
-    lowPowerOptions = lib.mkEnableOption "behavior settings for dealing with low battery";
+    power-config = lib.mkEnableOption "behavior settings for dealing with low battery";
   };
 }
