a0bc6385110857aa3ad30646bee94faad6f070da
Fernando RF <ferife@icloud.com>
2025-09-05 05:41:43 -0500

    Improve Mullvad VPN declaratioin

--- nixos/modules/packages/gui-apps/mullvad.nix
+++ nixos/modules/packages/gui-apps/mullvad.nix
@@ -5,12 +5,12 @@
   ...
 }: {
   config = lib.mkIf config.nixos.mullvadVpn {
-    environment.systemPackages = with pkgs; [
-      mullvad # CLI
-      mullvad-vpn # GUI
-    ];
     networking.wireguard.enable = true;
-    services.mullvad-vpn.enable = true;
+    services.mullvad-vpn = {
+      enable = true;
+      package = pkgs.mullvad-vpn; # Installs both CLI and GUI
+      enableExcludeWrapper = true;
+    };
     # networking.firewall.checkReversePath = "loose"; # Doen automatically by services.mullvad-vpn.enable = true;
   };
 }
