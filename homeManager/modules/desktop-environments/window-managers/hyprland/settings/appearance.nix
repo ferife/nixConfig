cbe09312cde6bd4eb4b137c04982cd5661dca8a2
Fernando RF <ferife@icloud.com>
2026-03-24 10:50:20 -0500

    Modify hypr appearance to better match noctalia

--- homeManager/modules/desktop-environments/window-managers/hyprland/settings/appearance.nix
+++ homeManager/modules/desktop-environments/window-managers/hyprland/settings/appearance.nix
@@ -10,10 +10,34 @@
           gaps_in = 5;
           gaps_out = 10;
         };
-        decoration = {
-          inactive_opacity = 0.8;
-        };
+
+        decoration.inactive_opacity = 0.85;
       }
+
+      (lib.mkIf (config.hm.wm.components.noctalia) {
+        general = {
+          gaps_in = 5;
+          gaps_out = 10;
+        };
+
+        decoration = {
+          rounding = 20;
+          rounding_power = 2;
+
+          shadow = {
+            enabled = true;
+            range = 4;
+            render_power = 3;
+          };
+
+          # blur = {
+          #   enabled = true;
+          #   size = 3;
+          #   passes = 2;
+          #   vibrancy = 0.1696;
+          # };
+        };
+      })
     ];
   };
 }
