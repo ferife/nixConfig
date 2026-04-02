4d4c15c32b8522f93f5e6040834a3bf29af414b4
Fernando RF <ferife@icloud.com>
2026-04-02 15:23:09 +0000

    Add hyprland keybinds

---
+++ modules/programs/desktop-environments/window-managers/hyprland/binds/open-programs.nix
@@ -0,0 +1,19 @@
+{inputs, ...}: {
+  # flake.modules.nixos.hyprland = {pkgs, ...}: {};
+
+  flake.modules.homeManager.hyprland = {
+    lib,
+    pkgs,
+    ...
+  }: {
+    wayland.windowManager.hyprland.settings = {
+      bindd = [
+        "SUPER, return, Open a terminal window, exec, ${lib.getExe pkgs.ghostty}"
+        "SUPER SHIFT, return, Open a browser window, exec, ${lib.getExe pkgs.librewolf}"
+        "SUPER, Space, Open the app launcher, exec, ${lib.getExe pkgs.rofi} -show drun"
+        "SUPER, O, Open obsidian.md, exec, ${lib.getExe pkgs.obsidian}"
+        "SUPER, O, Open the password manager, exec, ${lib.getExe pkgs.keepassxc}"
+      ];
+    };
+  };
+}
