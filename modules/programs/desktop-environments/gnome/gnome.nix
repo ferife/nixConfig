d9a91199667f15fb2c984d9716b98dc35c770a86
Fernando RF <ferife@icloud.com>
2026-04-02 14:05:50 +0000

    Add gnome browser connector

--- modules/programs/desktop-environments/gnome/gnome.nix
+++ modules/programs/desktop-environments/gnome/gnome.nix
@@ -2,7 +2,10 @@
   flake.modules.nixos.gnome = {pkgs, ...}: {
     # imports = [inputs.self.modules.nixos.gdm];
 
-    services.desktopManager.gnome.enable = true;
+    services = {
+      desktopManager.gnome.enable = true;
+      gnome.gnome-browser-connector.enable = true; # Allows for GNOME extensions to affect browsers
+    };
   };
 
   # flake.modules.homeManager.gnome = {pkgs, ...}: {};
