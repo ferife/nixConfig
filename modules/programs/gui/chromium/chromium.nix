ccf250ca2fc88a7a51f56a4634059f03aff39e23
Fernando RF <ferife@icloud.com>
2026-04-04 11:16:01 -0500

    Install chromium

---
+++ modules/programs/gui/chromium/chromium.nix
@@ -0,0 +1,14 @@
+{inputs, ...}: {
+  # flake.modules.nixos.chromium = {pkgs, ...}: {};
+
+  flake.modules.homeManager.chromium = {pkgs, ...}: {
+    programs.chromium = {
+      enable = true;
+      # extensions = [
+      #   # {id = "ddkjiahejlhfcafbddmgiahcphecmpfh";}
+      #   # User-Agent Switcher plugin: https://chromewebstore.google.com/detail/user-agent-switcher/dbclpoekepcmadpkeaelmhiheolhjflj
+      # ];
+    };
+    dconf.settings = {"org/gnome/shell".favorite-apps = ["chromium-browser.desktop"];};
+  };
+}
