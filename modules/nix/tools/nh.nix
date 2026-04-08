65a32834cb5e4eef8ac4ff43cd925c087c16d708
Fernando RF <ferife@icloud.com>
2026-04-08 13:44:31 -0500

    Improve nh declaration

--- modules/nix/tools/nh.nix
+++ modules/nix/tools/nh.nix
@@ -1,9 +1,15 @@
 {inputs, ...}: {
-  # flake.modules.nixos.nh = {pkgs, ...}: {};
+  flake.modules.nixos.nh = {pkgs, ...}: {
+    programs.nh = {
+      enable = true;
+      flake = "/home/fernandorf/Documents/Configs/nixConfig/nixConfig-main";
+    };
+  };
 
   flake.modules.homeManager.nh = {pkgs, ...}: {
     programs.nh = {
       enable = true;
+      flake = "/home/fernandorf/Documents/Configs/nixConfig/nixConfig-main";
     };
   };
 }
