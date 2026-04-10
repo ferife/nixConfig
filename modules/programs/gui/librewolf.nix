90b005119edb3a188ab5787f8fca4ff985305b75
Fernando RF <ferife@icloud.com>
2026-04-10 06:09:48 -0500

    Improve librewolf declaration

--- modules/programs/gui/librewolf.nix
+++ modules/programs/gui/librewolf.nix
@@ -6,6 +6,51 @@
   flake.modules.homeManager.librewolf = {pkgs, ...}: {
     programs.librewolf = {
       enable = true;
+      profiles.fernandorf = {
+        id = 0;
+        name = "fernandorf";
+        isDefault = true;
+        containersForce = true;
+        bookmarks = {
+          force = true;
+          settings = [
+            {
+              name = "Toolbar sites";
+              toolbar = true;
+              bookmarks = [
+                {
+                  name = "Doom Radio";
+                  url = "https://www.youtube.com/watch?v=JEuAYnjtJP0";
+                  keyword = "radio";
+                  tags = ["music"];
+                }
+                {
+                  name = "NixOS Search";
+                  url = "https://search.nixos.org/packages?channel=unstable";
+                  tags = ["nix" "search"];
+                }
+                {
+                  name = "Nix HM Search";
+                  url = "https://home-manager-options.extranix.com/?query=&release=master";
+                  tags = ["nix" "home manager" "search"];
+                }
+              ];
+            }
+          ];
+        };
+        # search = {};
+        #
+        # extensions = [
+        #   ublock origin
+        #   firefox multi account containers
+        #   temporary containers
+        #   keepassxc-browser
+        #   facebook container
+        #   mullvad (installed through mullvad.net)
+        #   tree-style tabs
+        #   unhook
+        # ];
+      };
     };
 
     stylix.targets.librewolf.colorTheme.enable = true;
