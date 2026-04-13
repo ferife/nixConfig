eb4be6107c8179b1a98325bec03ea1361f6f5051
Fernando RF <ferife@icloud.com>
2026-04-13 11:40:54 -0500

    Add list of possible noctalia plugins to install

--- modules/programs/desktop-environments/window-managers/components/noctalia/settings/noctalia-plugins/noctalia-plugins.nix
+++ modules/programs/desktop-environments/window-managers/components/noctalia/settings/noctalia-plugins/noctalia-plugins.nix
@@ -10,6 +10,18 @@
           name = "Official Noctalia Plugins";
           url = "https://github.com/noctalia-dev/noctalia-plugins";
         }
+        # Possible plugins to install
+        # gpu screen recorder
+        # timer
+        # screen toolkit
+        # obs control?
+        # workspace overview for hyprland
+        # show keys?
+        # kde connect?
+        # battery threshold control?
+        # polkit agent?
+        # web search
+        # unicode picker?
       ];
       # To identify the available settings of a plugin, look at the ~/.config/noctalia/plugins/<plugin-name>/Settings.qml file
       # In that file, there will be a few lines that start as "pluginApi.pluginSettings.<setting-name> = <bla bla bla>"
