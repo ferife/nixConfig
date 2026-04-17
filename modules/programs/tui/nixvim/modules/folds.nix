7c2562dea4dfc2ae5f4e6378f59498d54e81c159
Fernando RF <ferife@icloud.com>
2026-04-17 05:49:25 -0500

    Improve nvim statuscol declaration

--- modules/programs/tui/nixvim/modules/folds.nix
+++ modules/programs/tui/nixvim/modules/folds.nix
@@ -1,40 +1,51 @@
-{inputs, ...}: {
+{
+  inputs,
+  config,
+  ...
+}: {
   flake.modules.homeManager.nixvim-folds = {lib, ...}: {
     plugins = {
       nvim-ufo.enable = true;
       # origami.enable = true;
 
-      statuscol = {
-        enable = true;
-        settings = {
-          bt_ignore = null;
-          clickhandlers = {
-            FoldClose = "require('statuscol.builtin').foldclose_click";
-            FoldOpen = "require('statuscol.builtin').foldopen_click";
-            FoldOther = "require('statuscol.builtin').foldother_click";
-            Lnum = "require('statuscol.builtin').lnum_click";
+      statuscol = lib.mkMerge [
+        {
+          enable = true;
+          settings = {
+            bt_ignore = null;
+            clickhandlers = {
+              FoldClose = "require('statuscol.builtin').foldclose_click";
+              FoldOpen = "require('statuscol.builtin').foldopen_click";
+              FoldOther = "require('statuscol.builtin').foldother_click";
+              Lnum = "require('statuscol.builtin').lnum_click";
+            };
+            segments = [
+              # Sign column
+              {
+                text = ["%s"];
+                click = "v:lua.ScSa";
+              }
+
+              # Fold column
+              {
+                text = [{__raw = "require('statuscol.builtin').foldfunc";}];
+                click = "v:lua.ScFa";
+              }
+
+              # Line number column
+              {
+                click = "v:lua.ScLa";
+                condition = [true {__raw = "require('statuscol.builtin').not_empty";}];
+                text = [{__raw = "require('statuscol.builtin').lnumfunc";} " "];
+              }
+            ];
+            clickmod = "c";
+            ft_ignore = null;
+            setopt = true;
+            thousands = ".";
           };
-          segments = [
-            {
-              text = ["%s"];
-              click = "v:lua.ScSa";
-            }
-            {
-              text = [{__raw = "require('statuscol.builtin').foldfunc";}];
-              click = "v:lua.ScFa";
-            }
-            {
-              click = "v:lua.ScLa";
-              condition = [true {__raw = "require('statuscol.builtin').not_empty";}];
-              text = [{__raw = "require('statuscol.builtin').lnumfunc";} " "];
-            }
-          ];
-          clickmod = "c";
-          ft_ignore = null;
-          setopt = true;
-          thousands = ".";
-        };
-      };
+        }
+      ];
     };
 
     opts = {
