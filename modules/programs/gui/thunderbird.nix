2bd5d584bb1c8c94556e1c3b0a747d73a11dbd3d
Fernando RF <ferife@icloud.com>
2026-04-17 08:55:05 -0500

    Properly set up thunderbird with protonmail

--- modules/programs/gui/thunderbird.nix
+++ modules/programs/gui/thunderbird.nix
@@ -2,8 +2,12 @@
   # Thunderbird is an email client
 
   flake.modules.homeManager.thunderbird = {pkgs, ...}: {
-    programs.thunderbird = {
-      enable = true;
-    };
+    programs.thunderbird.enable = true;
+
+    # Add support for proton mail
+    home.packages = [
+      pkgs.protonmail-bridge
+      pkgs.protonmail-bridge-gui
+    ];
   };
 }
